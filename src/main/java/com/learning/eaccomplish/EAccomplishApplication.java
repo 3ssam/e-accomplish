03882ee57c7628be1e084d7dbe3e145f2f807d68
Mohamed Essam <mohamed.essam.mohamed.kamel@gmail.com>
2020-06-18 03:47:33 +0200

    Initialize project and set configurations

---
+++ src/main/java/com/learning/eaccomplish/EAccomplishApplication.java
@@ -0,0 +1,13 @@
+package com.learning.eaccomplish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EAccomplishApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EAccomplishApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/learning/eaccomplish/EAccomplishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.eaccomplish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EAccomplishApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
