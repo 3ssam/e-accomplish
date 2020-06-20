75d4bd9937f839d66bd8180b5c65d473b9bbee14
Mohamed Essam <mohamed.essam.mohamed.kamel@gmail.com>
2020-06-20 23:06:31 +0200

    Create Tables of Database

---
+++ src/main/java/com/learning/eaccomplish/models/Parent.java
@@ -0,0 +1,12 @@
+package com.learning.eaccomplish.models;
+
+import lombok.Getter;
+import lombok.Setter;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Setter
+@Getter
+@Entity
+public class Parent {
+}
---
+++ src/main/java/com/learning/eaccomplish/models/Report.java
@@ -0,0 +1,13 @@
+package com.learning.eaccomplish.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Setter
+@Getter
+@Entity
+public class Report {
+}
