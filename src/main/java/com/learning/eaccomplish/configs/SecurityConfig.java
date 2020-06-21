8902bef7eb507d6cabf74e97b69a0b109a6b9dc7
Mohamed Essam <mohamed.essam.mohamed.kamel@gmail.com>
2020-06-21 12:37:30 +0200

    User Details Service in Spring Security

--- src/main/java/com/learning/eaccomplish/configs/SecurityConfig.java
+++ src/main/java/com/learning/eaccomplish/configs/SecurityConfig.java
@@ -1,12 +1,16 @@
 package com.learning.eaccomplish.configs;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 import java.util.ArrayList;
@@ -16,11 +20,22 @@ import java.util.List;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+//    @Bean
+//    @Override
+//    protected UserDetailsService userDetailsService() {
+//        List<UserDetails> users = new ArrayList<>();
+//        users.add(User.withDefaultPasswordEncoder().username("essam").password("123456789").roles("ADMIN").build());
+//        return new InMemoryUserDetailsManager(users);
+//    }
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Bean
-    @Override
-    protected UserDetailsService userDetailsService() {
-        List<UserDetails> users = new ArrayList<>();
-        users.add(User.withDefaultPasswordEncoder().username("essam").password("123456789").roles("ADMIN").build());
-        return new InMemoryUserDetailsManager(users);
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        return provider;
     }
 }
---
+++ src/main/java/com/learning/eaccomplish/security/MyUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.learning.eaccomplish.security;
+
+import com.learning.eaccomplish.models.User;
+import com.learning.eaccomplish.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null)
+            throw new UsernameNotFoundException("this username is not exist");
+        return new UserPriencipal(user);
+    }
+
+}
