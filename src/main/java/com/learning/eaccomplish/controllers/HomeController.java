084f2e98327e28becb48b79ce867151069dc1ad9
Mohamed Essam <mohamed.essam.mohamed.kamel@gmail.com>
2020-06-26 18:18:16 +0200

    Create Edit or cancel payment

--- src/main/java/com/learning/eaccomplish/controllers/HomeController.java
+++ src/main/java/com/learning/eaccomplish/controllers/HomeController.java
@@ -2,7 +2,6 @@ package com.learning.eaccomplish.controllers;
 
 import com.learning.eaccomplish.models.Parent;
 import com.learning.eaccomplish.models.Payment;
-import com.learning.eaccomplish.models.User;
 import com.learning.eaccomplish.security.CurrentUser;
 import com.learning.eaccomplish.services.ParentService;
 import com.learning.eaccomplish.services.PaymentService;
@@ -16,7 +15,7 @@ import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/")
-//@SessionAttributes("currentUser")
+@SessionAttributes({"signupUser", "islogin"})
 public class HomeController {
 
     @Autowired
@@ -35,54 +34,67 @@ public class HomeController {
 //        return "AddPayment";
 //    }
 //
-//    @PostMapping("/payment")
-//    public String addPayment(@Valid Payment payment,  BindingResult result, Model model) {
-//        if (result.hasErrors())
-//            return "EditPayment";
-//        Parent parent = (Parent) model.getAttribute("currentUser");
-//        Payment tempPayment = paymentService.getPaymentByParent(parent);
-//        return "HomePage";
-//    }
-//
-//    @GetMapping("/payment")
-//    public String deactivatedPayment(@RequestParam(required = false) boolean active, Model model) {
-//        Parent parent = (Parent) model.getAttribute("currentUser");
-//        Payment payment = paymentService.getPaymentByParent(parent);
-//        payment.setActive(active);
-//        paymentService.EditPayment(payment,parent);
-//        return "HomePage";
-//    }
+    @PostMapping("/payment")
+    public String addPayment(@Valid Payment payment,  BindingResult result, Model model) {
+        if (result.hasErrors())
+            return "EditPayment";
+        Parent parent = CurrentUser.getUser();
+        paymentService.EditPayment(payment,parent);
+        //Payment tempPayment = paymentService.getPaymentByParent(parent);
+        return "HomePage";
+    }
 
+    @GetMapping("/payment")
+    public String deactivatedPayment(@RequestParam(required = false) boolean active, Model model) {
+        Parent parent = CurrentUser.getUser();
+        Payment payment = CurrentUser.getUser().getPayment();
+        paymentService.cancelPayment(payment,parent);
+        return "HomePage";
+    }
 
 
     @GetMapping()
-    public String home(@RequestParam(required = false) String choose,Model model) {
-        User user = CurrentUser.getUser();
-        Parent parent = parentService.getParent(CurrentUser.getId());
+    public String home(@RequestParam(required = false) String choose, Model model) {
+        Parent parent = CurrentUser.getUser();
         if (choose == null)
             return "HomePage";
-//        if (choose.equalsIgnoreCase("Deactivated")){
-//            parent.setActivated(false);
-//            parentService.EditParent(parent);
-//            return "logout";
-//        }
-//        else if (choose.equalsIgnoreCase("Payment")){
-//            Payment payment = paymentService.getPaymentByParent(parent);
-//            model.addAttribute("payment",payment);
-//            return "EditPayment";
-//        }
+        if (choose.equalsIgnoreCase("Deactivated")) {
+            parent.setActivated(false);
+            parentService.EditParent(parent);
+            model.addAttribute("isLocked", true);
+            return "redirect:" + "/logout";
+        } else if (choose.equalsIgnoreCase("Payment")) {
+            Payment payment = parent.getPayment();
+            if (payment == null)
+                payment = paymentService.getPaymentByParent(parent);
+            if (payment == null) {
+                model.addAttribute("signupUser", parent);
+                payment = new Payment();
+                payment.setParent(parent);
+                model.addAttribute("payment", payment);
+                model.addAttribute("islogin", true);
+                return "AddPayment";
+            }
+            model.addAttribute("payment", payment);
+            return "EditPayment";
+        }
 //        else if (choose.equalsIgnoreCase("Children"));
 //        else if (choose.equalsIgnoreCase("Quiz"));
         return "HomePage";
     }
 
     @RequestMapping("login")
-    public String login() {
+    public String login(Model model) {
+        Object o = model.getAttribute("isLocked");
+        if (o != null)
+            model.addAttribute("isLocked", true);
         return "Login";
     }
 
     @RequestMapping("logout")
-    public String logout() {
+    public String logout(Model model) {
+        if (model.getAttribute("isLocked") != null)
+            model.addAttribute("isLocked", true);
         return "Login";
     }
 
--- src/main/java/com/learning/eaccomplish/controllers/SignUpController.java
+++ src/main/java/com/learning/eaccomplish/controllers/SignUpController.java
@@ -2,6 +2,7 @@ package com.learning.eaccomplish.controllers;
 
 import com.learning.eaccomplish.models.Parent;
 import com.learning.eaccomplish.models.Payment;
+import com.learning.eaccomplish.security.CurrentUser;
 import com.learning.eaccomplish.services.ParentService;
 import com.learning.eaccomplish.services.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +15,7 @@ import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/signup")
-@SessionAttributes("signupUser")
+@SessionAttributes({"signupUser", "islogin"})
 public class SignUpController {
 
     @Autowired
@@ -29,25 +30,37 @@ public class SignUpController {
         if (result.hasErrors())
             return "AddParent";
         if (parentService.mailIsExist(parent)) {
-            model.addAttribute("isExist",true);
+            model.addAttribute("isExist", true);
             return "AddParent";
         }
         parentService.createParent(parent);
-        model.addAttribute("signupUser",parent);
+        model.addAttribute("signupUser", parent);
         Payment payment = new Payment();
         payment.setParent(parent);
-        model.addAttribute("payment",payment);
+
+        model.addAttribute("payment", payment);
+        model.addAttribute("islogin", false);
         return "AddPayment";
     }
 
     @PostMapping("/payment")
-    public String addPayment(@Valid Payment payment,  BindingResult result, Model model) {
+    public String addPayment(@Valid Payment payment, BindingResult result, Model model) {
         if (result.hasErrors())
             return "AddPayment";
         Parent parent = (Parent) model.getAttribute("signupUser");
+        Object islogin = model.getAttribute("islogin");
+        if (parent == null) {
+            parent = CurrentUser.getUser();
+            islogin = true;
+        }
         payment.setParent(parent);
-        paymentService.createPayment(payment,parent);
-        return "redirect:" + "/login";
+        paymentService.createPayment(payment, parent);
+        if (islogin == null)
+            return "redirect:" + "/";
+        if ((boolean) islogin == true)
+            return "redirect:" + "/";
+        else
+            return "redirect:" + "/login";
     }
 
 
