1c4c699d89225cec4d76b700a2985432b63603b0
Mohamed Essam <mohamed.essam.mohamed.kamel@gmail.com>
2020-06-27 10:43:33 +0200

    Quiz Story as Functionality

--- src/main/java/com/learning/eaccomplish/controllers/HomeController.java
+++ src/main/java/com/learning/eaccomplish/controllers/HomeController.java
@@ -1,13 +1,9 @@
 package com.learning.eaccomplish.controllers;
 
-import com.learning.eaccomplish.models.Child;
-import com.learning.eaccomplish.models.Parent;
-import com.learning.eaccomplish.models.Payment;
+import com.learning.eaccomplish.models.*;
 import com.learning.eaccomplish.repositories.ChildRepository;
 import com.learning.eaccomplish.security.CurrentUser;
-import com.learning.eaccomplish.services.ChildService;
-import com.learning.eaccomplish.services.ParentService;
-import com.learning.eaccomplish.services.PaymentService;
+import com.learning.eaccomplish.services.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,11 +12,12 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.websocket.server.PathParam;
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
 @RequestMapping("/")
-@SessionAttributes({"signupUser", "islogin","currentChild"})
+@SessionAttributes({"signupUser", "islogin", "currentChild", "currentQuestion", "currentQuiz", "currentReport"})
 public class HomeController {
 
     @Autowired
@@ -31,6 +28,12 @@ public class HomeController {
 
     @Autowired
     private ChildService childService;
+
+    @Autowired
+    private ReportService reportService;
+
+    @Autowired
+    private ResultService resultService;
 
 
     @PostMapping("/child")
@@ -43,7 +46,9 @@ public class HomeController {
     }
 
     @PostMapping("login/child")
-    public String childLogin(@Valid Child child, Model model) {
+    public String childLogin(@Valid Child child, BindingResult result, Model model) {
+        if (result.hasErrors())
+            return "OpenQuiz";
         Parent parent = parentService.getParent(CurrentUser.getId());
         Child existChild = childService.getChild(child.getName(), child.getPinCode(), parent);
         if (existChild == null) {
@@ -51,7 +56,7 @@ public class HomeController {
             return "OpenQuiz";
         }
         model.addAttribute("currentChild", existChild);
-        return "HomePage";
+        return "StartQuiz";
     }
 
 
@@ -147,5 +152,104 @@ public class HomeController {
         return "HomePage";
     }
 
+    @RequestMapping("quiz")
+    public String startQuiz(Model model) {
+        Child child = (Child) model.getAttribute("currentChild");
+        Question question = child.getLastQuestion();
+        Quiz quiz = child.getLastQuiz();
+        model.addAttribute("currentQuestion", question);
+        model.addAttribute("currentQuiz", quiz);
+        String fullQuestion = question.getFullQuestion() + " = ";
+        model.addAttribute("fullQuestion", fullQuestion);
+        model.addAttribute("next", false);
+        Report report = reportService.getReport(child, quiz);
+        List<Report> existReport = reportService.getReport(child, LocalDate.now().toString());
+        if (report == null)
+            report = reportService.createReport(child, quiz);
+        if (CheckTest(report, existReport)) {
+            model.addAttribute("finishQuiz", true);
+            return "StartQuiz";
+        }
+        Result result = new Result();
+        result.setQuestion(question);
+        result.setReport(report);
+        model.addAttribute("currentReport", report);
+        model.addAttribute("result", result);
+        return "Quiz";
+    }
+
+
+    @PostMapping("answer")
+    public String answer(@Valid Result result, BindingResult bindingResult, Model model) {
+        Child child = (Child) model.getAttribute("currentChild");
+        Question question = (Question) model.getAttribute("currentQuestion");
+        Quiz quiz = (Quiz) model.getAttribute("currentQuiz");
+        Report report = (Report) model.getAttribute("currentReport");
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("next", false);
+            model.addAttribute("fullQuestion", question.getFullQuestion() + " = ");
+            return "Quiz";
+        }
+
+        long answer = Long.parseLong(result.getState());
+        if (answer == question.getActualResult()) {
+            result = resultService.createResult(question, report, "Right");
+            model.addAttribute("answer", true);
+            model.addAttribute("next", true);
+            report.setNumOfRight(report.getNumOfRight() + 1);
+            report.setTotalNumber(report.getTotalNumber() + 1);
+        } else {
+            result = resultService.createResult(question, report, "Wrong");
+            model.addAttribute("answer", false);
+            model.addAttribute("next", true);
+            report.setNumOfWrong(report.getNumOfWrong() + 1);
+            report.setTotalNumber(report.getTotalNumber() + 1);
+        }
+        reportService.editReport(report);
+        question = resultService.getNextQuestion(question);
+        child.setLastQuestion(question);
+        if (report.getTotalNumber() == 30) {
+            quiz = resultService.getNextQuiz(quiz);
+            child.setLastQuiz(quiz);
+            childService.EditChild(child);
+            if (report.getNumOfRight() == 30)
+                report.setState("Excellent");
+            else if (report.getNumOfRight() >= 15)
+                report.setState("Good");
+            else
+                report.setState("Poor");
+            reportService.editReport(report);
+            return "EndQuiz";
+        }
+        model.addAttribute("currentReport", report);
+        model.addAttribute("currentQuestion", question);
+        model.addAttribute("currentQuiz", quiz);
+        childService.EditChild(child);
+        model.addAttribute("currentChild", child);
+        return "Quiz";
+//        Parent parent = parentService.getParent(CurrentUser.getId());
+//        Child existChild = childService.getChild(child.getName(), child.getPinCode(), parent);
+//        if (existChild == null) {
+//            model.addAttribute("LoginError", true);
+//            return "OpenQuiz";
+//        }
+//        model.addAttribute("currentChild", existChild);
+        //return "redirect:" + "/quiz";
+    }
+
+    private boolean CheckTest(Report report, List<Report> reports) {
+        if (reports == null)
+            return false;
+        if (reports.size() == 1 && reports.get(0).getQuiz().getId() == report.getQuiz().getId())
+            return false;
+        if (reports.size() == 1 && reports.get(0).getQuiz().getId() != report.getQuiz().getId() && reports.get(0).getState().equalsIgnoreCase("Excellent"))
+            return false;
+        if (reports.size() == 2) {
+            for (int i = 0; i < reports.size(); i++)
+                if (reports.get(i).getQuiz().getId() == report.getQuiz().getId())
+                    return false;
+        }
+        return true;
+    }
 
 }
